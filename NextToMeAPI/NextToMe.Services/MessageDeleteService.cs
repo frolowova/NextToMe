83dab14b0eed8b838690841ab09ef775463e7ba5
Ishakow Maksim <ishakowm@mail.ru>
2020-10-25 18:30:55 +0300

    Removing unnecessary using

--- NextToMeAPI/NextToMe.Services/MessageDeleteService.cs
+++ NextToMeAPI/NextToMe.Services/MessageDeleteService.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NextToMe.Database;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Internal;
-using Microsoft.Extensions.DependencyInjection;
-using NextToMe.Database.Entities;
 
 namespace NextToMe.Services
 {
