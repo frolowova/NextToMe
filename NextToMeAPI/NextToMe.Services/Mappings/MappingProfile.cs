b811e35f03427136a5b11152f31f34a8c27e23d5
Ishakow Maksim <ishakowm@mail.ru>
2020-11-29 01:44:09 +0300

    Minor fix

--- NextToMeAPI/NextToMe.Services/Mappings/MappingProfile.cs
+++ NextToMeAPI/NextToMe.Services/Mappings/MappingProfile.cs
@@ -17,7 +17,7 @@ namespace NextToMe.Services.Mappings
             CreateMap<Message, MessageResponse>()
                 .ForMember(x => x.From, opt => opt.MapFrom(y => y.User.UserName))
                 .ForMember(x => x.Location, opt => opt.MapFrom(y => new Common.Models.Location(y.Location.X, y.Location.Y)))
-                .ForMember(x => x.LikesCount, opt => opt.MapFrom(y => y.UserLikedMessages.Select(z => z.Message).Count()))
+                .ForMember(x => x.LikesCount, opt => opt.MapFrom(y => y.UserLikedMessages.ToList().Count))
                 .ReverseMap();
 
             CreateMap<AddMessageCommentRequest, MessageComment>().ReverseMap();
