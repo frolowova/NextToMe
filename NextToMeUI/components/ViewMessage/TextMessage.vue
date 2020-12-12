e71d535a0431a17ae5cb95f234f9c2322ef74279
Peleshok Ivan <peleshok.ivan@gmail.com>
2020-12-12 18:05:50 +0300

    fixed overflow

--- NextToMeUI/components/ViewMessage/TextMessage.vue
+++ NextToMeUI/components/ViewMessage/TextMessage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-2">
-    <p v-html="text(message)" />
+    <p class="tag-message" v-html="text(message)" />
   </div>
 </template>
 
@@ -16,3 +16,31 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.tag-message {
+  margin-right: 1rem;
+  overflow-x: auto;
+}
+::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+  margin-left: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #1e1f2044;
+  border-radius: 5px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  opacity: 0.5;
+}
+</style>
