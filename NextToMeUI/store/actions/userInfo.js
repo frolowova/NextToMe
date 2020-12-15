04a7387218e6118bd2a7d04affcb920b860e5001
OitaGG <oitag@yandex.ru>
2020-12-15 03:00:17 +0300

    Fix email bag first time authorization

--- NextToMeUI/store/actions/userInfo.js
+++ NextToMeUI/store/actions/userInfo.js
@@ -1,4 +1,4 @@
 export const GET_USER_INFO = "GET_USER_INFO";
 export const SEND_USER_INFO = "SEND_USER_INFO";
 export const SEND_USER_PHOTO = "SEND_USER_PHOTO";
-export const SET_USER_ID = "SET_USER_ID";
+export const SET_AUTH_DATA = "SET_AUTH_DATA";
